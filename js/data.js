32c47f61418cebf453f9445800cbcbd9856cce7f
Débora Setton <debora.setton@infosimples.com.br>
2014-03-18 15:38:27 -0300

    Solution for Issue#9

---
+++ js/data.js
@@ -0,0 +1,131 @@
+/*
+ * This file contains information about people.
+ * Example:
+ * {
+ *   full_name: 'Patrícia Lima',
+ *   first_name: 'Patrícia',
+ *   course: 'Curso Cooperativo',
+ *   birthday: '26/08/1988',
+ *   about: 'I love listening to music and doing nothing at home.',
+ *   picture: 'cb8405ed12689a3e139d9368b06a4a33'
+ * }
+ */
+
+people = [
+  {
+    full_name: 'Adriano Dennanni',
+    first_name: 'Adriano'
+  },
+  {
+    full_name: 'Alan Raso',
+    first_name: 'Alan'
+  },
+  {
+    full_name: 'André Amaral de Sousa',
+    first_name: 'André'
+  },
+  {
+    full_name: 'Bruno Henrique da Silva',
+    first_name: 'Bruno'
+  },
+  {
+    full_name: 'Cesar Rodrigues dos Passos',
+    first_name: 'Cesar'
+  },
+  {
+    full_name: 'Felipe Cardoso Resende',
+    first_name: 'Felipe'
+  },
+  {
+    full_name: 'Frederico Augusto Vieira Campos de Castro',
+    first_name: 'Frederico'
+  },
+  {
+    full_name: 'Gabriel Souza Victorino Lopes',
+    first_name: 'Gabriel'
+  },
+  {
+    full_name: 'Guilherme Job Rocha Silveira',
+    first_name: 'Guilherme'
+  },
+  {
+    full_name: 'Ian Elmôr Lang',
+    first_name: 'Ian'
+  },
+  {
+    full_name: 'Isabel Rosalino Gomes',
+    first_name: 'Isabel'
+  },
+  {
+    full_name: 'Ivan Diego de sousa',
+    first_name: 'Ivan'
+  },
+  {
+    full_name: 'João Batista Marinho da Silva Filho',
+    first_name: 'João Batista'
+  },
+  {
+    full_name: 'João Henrique Kersul Faria',
+    first_name: 'João Henrique'
+  },
+  {
+    full_name: 'Lucas Batista Gabriel',
+    first_name: 'Lucas'
+  },
+  {
+    full_name: 'Luiz Fernando Pessoa Lustosa Cabral',
+    first_name: 'Luiz'
+  },
+  {
+    full_name: 'Marcos Teruaki Hosoya',
+    first_name: 'Marcos'
+  },
+  {
+    full_name: 'Martin Andrade Jastrzebski',
+    first_name: 'Martin'
+  },
+  {
+    full_name: 'Matheus Eis Pagani',
+    first_name: 'Matheus Pagani'
+  },
+  {
+    full_name: 'Matheus Santos Oliveira',
+    first_name: 'Matheus Oliveira'
+  },
+  {
+    full_name: 'Mayer Rangel Levy',
+    first_name: 'Mayer'
+  },
+  {
+    full_name: 'Nsunda Bondo Lubaki',
+    first_name: 'Nsunda'
+  },
+  {
+    full_name: 'Pedro Napolitano Sant\'Ana',
+    first_name: 'Pedro'
+  },
+  {
+    full_name: 'Rafael Coregliano Ring',
+    first_name: 'Rafael Ring'
+  },
+  {
+    full_name: 'Rafael Freitas da Silva',
+    first_name: 'Rafael Freitas'
+  },
+  {
+    full_name: 'Rafael Trostli Costella',
+    first_name: 'Rafael Costella'
+  },
+  {
+    full_name: 'Ricardo Boccoli Gallego',
+    first_name: 'Ricardo'
+  },
+  {
+    full_name: 'Saulo de Tarso Ribeiro Costa',
+    first_name: 'Saulo'
+  },
+  {
+    full_name: 'Victor Gabriel Maraccini',
+    first_name: 'Victor'
+  }
+];
